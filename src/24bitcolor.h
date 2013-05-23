c991cee43f69106e05782c79e3ec03a86be9a194
Benjamin R. Haskell <urxvt@benizi.com>
2013-05-23 03:06:09 -0400

    wip: adds RXVT_TO_24BIT

--- src/24bitcolor.h
+++ src/24bitcolor.h
@@ -7,6 +7,14 @@
 #error only XFT for now
 #endif
 
+#define RXVT_TO_24BIT(xft,out) \
+  out = 0; \
+  out += (xft.c.color.red >> 8) & 0xff; \
+  out <<= 8; \
+  out += (xft.c.color.green >> 8) & 0xff; \
+  out <<= 8; \
+  out += (xft.c.color.blue >> 8) & 0xff
+
 #define XFT_COLOR(i,n) \
   XftColor n; \
   n.color.red = (i >> 16) & 0xff; \
@@ -16,10 +24,10 @@
   n.color.green *= 0x101; \
   n.color.blue *= 0x101; \
   n.color.alpha = 0xffff; \
-  n.pixel = n.color.alpha << 48 \
-    + n.color.red << 32 \
-    + n.color.green << 16 \
-    + n.color.blue; \
+  n.pixel = (((unsigned long)n.color.alpha) << 48) \
+    + (((unsigned long)n.color.red) << 32) \
+    + (((unsigned long)n.color.green) << 16) \
+    + ((unsigned long)n.color.blue); \
   if((i < 512) && (i > 1)) { \
   fprintf(stderr, "COLOR %d -> %d,%d,%d\n", i, n.color.red, n.color.green, n.color.blue); \
   }
